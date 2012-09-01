3f680b71023cbd27211893bb68eceb6d87eedde5
Dominic Tarr <dominic.tarr@gmail.com>
2012-09-01 23:34:53 +0200

    fix: remove graphs when stream ends

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+'use strict';
+
 var reconnect = require('reconnect')
 var ss        = require('smoothie-stream')
 var MuxDemux  = require('mux-demux')
@@ -5,15 +7,19 @@ var ready     = require('domready')
 var h         = require('h')
 var u         = require('./util')
 
-function getCanvas (id) {
-  var canvas = document.getElementById(id)
+function toCanvas (stream) {
+  var canvas = document.getElementById(stream.meta.name)
+    ,  parent
   if(!canvas) {
    document.getElementById('graphs')
       .appendChild(
-      h('div', h('lable', id),
+      parent = h('div', h('lable', id),
         canvas = h('canvas#'+id, { width: '600px', height: '200px' })
       )
     )
+    u.onTerminate(stats, function () {
+      parent.parentElement.removeChild(canvas)
+    })
   }
   return canvas
 }
@@ -25,14 +31,25 @@ function getCanvas (id) {
 
 ready(function () {
   reconnect(function (shoe) {
-    shoe.pipe(MuxDemux(function (stats) {
-      var canvas = getCanvas(stats.meta.name)
-      var smoothie = ss(stats.meta)
-      smoothie.streamTo(canvas, 1e3)
-      u.onTerminate(stats, function () {
-        canvas.parentElement.removeChild(canvas)
-      })
+    shoe.pipe(MuxDemux(function toCanvas (stream) {
+      var canvas = document.getElementById(stream.meta.name)
+      var container
+      var id = stream.meta.name
+      if(!canvas) {
+       document.getElementById('graphs')
+          .appendChild(
+          container = h('div', h('label', id), h('br'),
+            canvas = h('canvas#'+id, { width: '600px', height: '200px' })
+          )
+        )
+        var s = ss(stream.meta)
+        stream.pipe(s)
+        s.streamTo(canvas, 1e3)
 
+        u.onTerminate(stream, function () {
+          container.parentElement.removeChild(container)
+        })
+      }
     })).pipe(shoe)
   }).connect('/monitor')
 })
